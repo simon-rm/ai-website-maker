006ccb88a8c36da5c0ad54cdbfa88064cd684b2b
simon murcia <srisnikmurcia@itba.edu.ar>
2024-04-15 00:57:32 -0300

    website controller basic behaviour

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,11 +1,29 @@
 class WebsitesController < ApplicationController
   def new
+    @website = Website.new
   end
 
   def show
+    @website = Website.find params[:id]
   end
 
   def create
+    @website = Website.new website_params
+    @website.content = get_content
+    if @website.save
+      redirect_to @website
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
+  private
+
+  def website_params
+    params.require(:website).permit(:name, :description)
+  end
+
+  def get_content
+    "<h1>Some content</h1>"
   end
 end
